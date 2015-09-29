9cc74633d8711dfe7fe650e5fedd4b043ec0d1b6
Joel Fillmore <joelfillmore@gmail.com>
2015-09-29 13:18:07 -0700

    Coloring Book Edge Demo

---
+++ coloringbook/js/app.js
@@ -0,0 +1,28 @@
+(function(){
+(function() {
+
+    function scrollTo(top) {
+        $('html, body').animate({ scrollTop: top}, 800, 'easeInOutCirc');
+    }
+
+    $('#top').click(function() {
+        scrollTo(0);
+    });
+
+    $('.tile').click(function() {
+        var id = $(this).attr('href'),
+            offset = $(id).offset().top;
+        scrollTo($(id).offset().top);
+        return false;
+    });
+
+    $(window).scroll(function(e) {
+        var top = $('body').scrollTop(),
+            height = $(window).height();
+        $('#top').toggleClass('showing', top > height);
+    });
+
+})();
+
+
+})();
