610722ed7617823d2f86f8d203b4b4b954735d7f
molant <amolleda@gmail.com>
2016-01-04 11:51:19 +0100

    Configuring project to use travis

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = function (grunt) {
+	require('time-grunt')(grunt);
+	// Load grunt tasks automatically
+	require('load-grunt-tasks')(grunt);
+
+	grunt.initConfig({
+		eslint: {
+			demos: {
+				files: {
+					src: ['**/*.js']
+				},
+				options: {
+					configFile: '.eslintrc.json',
+					ignorePath: '.eslintignore',
+					quiet: true
+				}
+			}
+		}
+	});
+
+	grunt.registerTask('test', ['eslint']);
+	grunt.registerTask('default', 'test');
+};
