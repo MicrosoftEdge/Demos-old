d5031232fb153cbf74a077b6e677248ba05150c5
Jesus David García Gomez <dgarcia@plainconcepts.com>
2017-01-05 10:42:26 -0800

    Fixed focus on Audiomixer demo's popup (#275)
    * Fixed focus on Audiomixer demo's popup
    
    * Refactored toArray function in audiomixer

--- audiomixer/scripts/ui.js
+++ audiomixer/scripts/ui.js
@@ -1,6 +1,10 @@
 /* global $, audio, db */
-(function() {
+(function () {
 	'use strict';
+
+	var toArray = function (collection) {
+		return [].slice.call(collection);
+	};
 
 	var ui = {
 		ui: this,
@@ -12,39 +16,39 @@
 
 		html: {
 			trackRow: '<li class="track" id="track-{NAME}" data-index="{INDEX}">'
-					+ '<div class="controls">'
-					+ '<a href="#" class="toggler"></a>'
-					+ '<div class="label">{TITLE}</div>'
-					+ '<input class="volume" type="range" min="0" value="50" max="100" step="5" />'
-					+ '</div>'
-					+ '<ul class="grid">{GRID}</ul>'
-					+ '</li>',
+			+ '<div class="controls">'
+			+ '<a href="#" class="toggler"></a>'
+			+ '<div class="label">{TITLE}</div>'
+			+ '<input class="volume" type="range" min="0" value="50" max="100" step="5" />'
+			+ '</div>'
+			+ '<ul class="grid">{GRID}</ul>'
+			+ '</li>',
 
-			gridItem: '<li><span></span></li>',			
+			gridItem: '<li><span></span></li>',
 
 			tableRow: '<tr class="table-row{CLASS}" data-id="{ID}">'
-					+ '<td>{NAME}</td>'
-					+ '<td>{AUTHOR}</td>'
-					+ '<td>{GENRE}</td>'
-					+ '<td><a href="#" class="button-delete close">&times;</a></td>'
-					+ '</tr>',
+			+ '<td>{NAME}</td>'
+			+ '<td>{AUTHOR}</td>'
+			+ '<td>{GENRE}</td>'
+			+ '<td><a href="#" class="button-delete close">&times;</a></td>'
+			+ '</tr>',
 
-			diagOver: '<div id="dialog">'
-					+ '<div class="overlay"></div>'
-					+ '<div class="dialog">'
-					+ '<div class="header">'
-					+ '<a href="#" class="close">&times;</a>'
-					+ '<h4 class="title"></h4>'
-					+ '</div>'
-					+ '<div class="body">'
-					+ '<p></p>'
-					+ '</div>'
-					+ '<div class="footer">'
-					+ '<button type="button" class="button-dismiss">Cancel</button>'
-					+ '<button type="button" class="button-confirm">OK</button>'
-					+ '</div>'
-					+ '</div>'
-					+ '</div>'
+			diagOver: '<div id="dialog" role="alertdialog" aria-labelledby="modal-title" aria-describedby="modal-body">'
+			+ '<div class="overlay"></div>'
+			+ '<div class="dialog" tabindex="-1">'
+			+ '<div class="header">'
+			+ '<a href="#" class="close">&times;</a>'
+			+ '<h4 class="title" id="modal-title"></h4>'
+			+ '</div>'
+			+ '<div class="body" id="modal-body">'
+			+ '<p></p>'
+			+ '</div>'
+			+ '<div class="footer">'
+			+ '<button type="button" class="button-dismiss">Cancel</button>'
+			+ '<button type="button" class="button-confirm">OK</button>'
+			+ '</div>'
+			+ '</div>'
+			+ '</div>'
 		},
 
 		strings: {
@@ -63,8 +67,8 @@
 			playing: 'Playing...'
 		},
 
-		init: function() {
-			$(document).ready(function() {
+		init: function () {
+			$(document).ready(function () {
 				audio.init();
 				db.init();
 
@@ -74,9 +78,9 @@
 			});
 		},
 
-		bindEvents: function() {
-			$('#button-new').click(function() {
-				ui.getPermissionForUnsavedChanges().then(function(confirm) {
+		bindEvents: function () {
+			$('#button-new').click(function (e) {
+				ui.getPermissionForUnsavedChanges(e.target).then(function (confirm) {
 					if (confirm) {
 						audio.open();
 					}
@@ -84,13 +88,13 @@
 			});
 
 			if (audio.context) {
-				$('#play-button').click(function(event) {
+				$('#play-button').click(function (event) {
 					event.preventDefault();
 
 					audio.toggle();
 				});
 
-				$('.volume').change(function() {
+				$('.volume').change(function () {
 					var $this = $(this);
 					var tracks = audio.savedSet.settings.tracks;
 					var volume = parseInt($this.val(), 10);
@@ -108,7 +112,7 @@
 					ui.setUnsavedChanges(true);
 				});
 
-				$('.tempo').change(function() {
+				$('.tempo').change(function () {
 					var tempo = parseInt($(this).val(), 10);
 
 					audio.savedSet.settings.tempo = tempo;
@@ -120,7 +124,7 @@
 				ui.alert(ui.strings.upgrade_browser);
 			}
 
-			$('#dash').on('click', 'a.toggler, .grid li', function(e) {
+			$('#dash').on('click', 'a.toggler, .grid li', function (e) {
 				e.preventDefault();
 
 				var $this = $(this);
@@ -133,20 +137,20 @@
 					$this.parents('.track').toggleClass('disabled');
 
 					tracks[track].active = !$this.hasClass('off');
-				// otherwise a grid item was clicked
+					// otherwise a grid item was clicked
 				} else {
 					$this.toggleClass(ui.classes.selected);
 
 					var note = $this.parent().children().index($this);
 
-					tracks[track].notes[note] = $this.hasClass(ui.classes.selected);			
+					tracks[track].notes[note] = $this.hasClass(ui.classes.selected);
 				}
 
 				ui.setUnsavedChanges(true);
 			});
 
 			$('#save-form')
-				.submit(function(event) {
+				.submit(function (event) {
 					event.preventDefault();
 
 					if (!audio.unsavedChanges) {
@@ -166,47 +170,47 @@
 
 					if (emptyFields.length > 0) {
 						ui.alert(ui.strings.provide_following
-								+ '<br><ul><li>'
-								+ emptyFields.join('</li><li>')
-								+ '</li></ul>');
-						
+							+ '<br><ul><li>'
+							+ emptyFields.join('</li><li>')
+							+ '</li></ul>', document.getElementById('button-save'));
+
 						return false;
 					}
 
 					audio.savedSet.metadata = metadata;
 
-					db.add(audio.savedSet, audio.currentlyOpenedId).then(function(id) {
+					db.add(audio.savedSet, audio.currentlyOpenedId).then(function (id) {
 						audio.currentlyOpenedId = id;
 						ui.setUnsavedChanges(false);
 						ui.updateTable();
-					}, function(error) {
+					}, function (error) {
 						if (error === 'ConstraintError') {
 							ui.alert(ui.strings.no_same_name);
 						}
 					});
 				})
 				.children('input[name=name], input[name=author], input[name=genre]')
-					.keyup(function() {
-						var $this = $(this);
-
-						if ($this.val() !== audio.savedSet.metadata[$this.attr('name')]) {
-							ui.setUnsavedChanges(true);
-						}
-					})
-					.change(function() {
-						ui.setUnsavedChanges(true);
-					});
-
-			$('#saved-list')
-				.on('click', 'tbody tr:not(.active)', function() {
+				.keyup(function () {
 					var $this = $(this);
 
-					ui.getPermissionForUnsavedChanges().then(function(confirm) {
+					if ($this.val() !== audio.savedSet.metadata[$this.attr('name')]) {
+						ui.setUnsavedChanges(true);
+					}
+				})
+				.change(function () {
+					ui.setUnsavedChanges(true);
+				});
+
+			$('#saved-list')
+				.on('click', 'tbody tr:not(.active)', function () {
+					var $this = $(this);
+
+					ui.getPermissionForUnsavedChanges().then(function (confirm) {
 						if (confirm) {
 							var id = $this.data('id');
 
 							if (id > -1) {
-								db.get(id).then(function(set) {
+								db.get(id).then(function (set) {
 									audio.open(set, id);
 									$this.addClass('active');
 								});
@@ -214,19 +218,19 @@
 						}
 					});
 				})
-				.on('click', '.button-delete', function(e) {
+				.on('click', '.button-delete', function (e) {
 					e.preventDefault();
 					e.stopPropagation();
 					var $this = $(this);
 
-					ui.confirm(ui.strings.confirm_delete).then(function(confirm) {
+					ui.confirm(ui.strings.confirm_delete, e.target).then(function (confirm) {
 						if (!confirm) {
 							return;
 						}
 
 						var id = $this.parents('tr').data('id');
 
-						db.remove(id).then(function() {
+						db.remove(id).then(function () {
 							if (id === audio.currentlyOpenedId) {
 								ui.setUnsavedChanges(true);
 								audio.currentlyOpenedId = -1;
@@ -237,21 +241,21 @@
 					});
 				});
 
-			$('#saved-list-explainer a').click(function(e) {
+			$('#saved-list-explainer a').click(function (e) {
 				e.preventDefault();
 				db.populate();
 			});
 		},
 
-		getPermissionForUnsavedChanges: function() {
+		getPermissionForUnsavedChanges: function (originator) {
 			audio.pause();
 
 			return audio.unsavedChanges ?
-					ui.confirm(ui.strings.lose_all_changes) :
-					Promise.resolve(true);
+				ui.confirm(ui.strings.lose_all_changes, originator) :
+				Promise.resolve(true);
 		},
 
-		drawTracks: function() {
+		drawTracks: function () {
 			var tracks = audio.savedSet.settings.tracks;
 			var html = '';
 
@@ -261,8 +265,8 @@
 				trackHTML = trackHTML.replace('{INDEX}', i);
 				trackHTML = trackHTML.replace('{NAME}', tracks[i].name);
 				trackHTML = trackHTML.replace('{TITLE}', tracks[i].name.charAt(0).toUpperCase()
-														+ tracks[i].name.substr(1).toLowerCase());
-				
+					+ tracks[i].name.substr(1).toLowerCase());
+
 				var gridHTML = '';
 				for (var j = 0; j < audio.notes; j++) {
 					gridHTML += this.html.gridItem;
@@ -276,7 +280,7 @@
 			$('#dash').html(html);
 		},
 
-		playPause: function(play) {
+		playPause: function (play) {
 			if (!play) {
 				$('#play-button .icon-play').show();
 				$('#play-button .icon-pause').hide();
@@ -288,16 +292,16 @@
 			}
 		},
 
-		clearPlayhead: function(currentIndex) {
+		clearPlayhead: function (currentIndex) {
 			$('.grid li:nth-child(' + (currentIndex + 1) + ')').removeClass(this.classes.highlighted);
 		},
 
-		clearActiveRow: function() {
+		clearActiveRow: function () {
 			$('#saved-list tr.active').removeClass('active');
 		},
 
-		updateTable: function() {
-			return db.getConfigs().then(function(items) {
+		updateTable: function () {
+			return db.getConfigs().then(function (items) {
 				var html = '';
 
 				if (items.length === 0) {
@@ -323,7 +327,7 @@
 			});
 		},
 
-		updateTracks: function(savedSet) {
+		updateTracks: function (savedSet) {
 			var tracks = savedSet.settings.tracks;
 
 			$('.volume-master').val(savedSet.settings.volume);
@@ -347,19 +351,19 @@
 			}
 		},
 
-		movePlayheadForward: function(currentIndex, previousIndex) {
+		movePlayheadForward: function (currentIndex, previousIndex) {
 			$('.grid li:nth-child(' + (currentIndex + 1) + ')').addClass(this.classes.highlighted);
 			$('.grid li:nth-child(' + (previousIndex + 1) + ')').removeClass(this.classes.highlighted);
 		},
 
-		setUnsavedChanges: function(unsavedChanges) {
+		setUnsavedChanges: function (unsavedChanges) {
 			audio.unsavedChanges = unsavedChanges;
 
 			$('#save-form button[type=submit]').toggleClass('disabled', !audio.unsavedChanges);
 		},
 
-		alert: function(str) {
-			var diag = ui.createAndShowDiag(ui.strings.please_note, str);
+		alert: function (str, originator) {
+			var diag = ui.createAndShowDiag(ui.strings.please_note, str, originator);
 
 			$('.button-dismiss', diag).text(ui.strings.ok);
 			$('.button-confirm', diag).hide();
@@ -367,19 +371,19 @@
 			return diag;
 		},
 
-		confirm: function(str) {
-			var diag = ui.createAndShowDiag(ui.strings.please_confirm, str);
+		confirm: function (str, originator) {
+			var diag = ui.createAndShowDiag(ui.strings.please_confirm, str, originator);
 
 			$('.button-dismiss', diag).text(ui.strings.cancel);
 			$('.button-confirm', diag).show();
 
-			return new Promise(function(resolve) {
-				$('.button-confirm', diag).on('click.confirm', function() {
+			return new Promise(function (resolve) {
+				$('.button-confirm', diag).on('click.confirm', function () {
 					resolve(true);
 					ui.dialog.hide();
 				});
 
-				diag.on('hidden.dialog', function() {
+				diag.on('hidden.dialog', function () {
 					resolve(false);
 
 					// cleanup while we're at it
@@ -389,12 +393,13 @@
 			});
 		},
 
-		createAndShowDiag: function(title, msg) {
+		createAndShowDiag: function (title, msg, originator) {
 			var diag = ui.dialog.init();
 
 			ui.dialog.setTitle(title);
 			ui.dialog.setText(msg);
 
+			ui.dialog.originator = originator;
 			ui.dialog.show();
 
 			return diag;
@@ -404,27 +409,68 @@
 			el: null,
 			active: false,
 			overlay: null,
+			dialog: null,
+			focusableArray: [],
+			focusIndex: 0,
+			originator: null,
 
-			init: function() {
+			init: function () {
 				if (!this.el) {
 					this.el = $(ui.html.diagOver).appendTo('#audiomixer');
 					this.overlay = $('.overlay', this.el);
-
+					this.dialog = $('.dialog', this.el);
 					this.overlay.add('.button-dismiss, .close', this.el)
-						.on('click.dismiss', function(e) {
+						.on('click.dismiss', function (e) {
 							e.preventDefault();
 							ui.dialog.hide();
 						});
+
+					var tabFocus = function (e) {
+						e.preventDefault();
+						if (ui.dialog.focusableArray.length === 1) {
+							ui.dialog.focusableArray[0].focus();
+						} else if (ui.dialog.focusableArray.length > 1) {
+							if (e.shiftKey) {
+								ui.dialog.focusIndex--;
+							} else {
+								ui.dialog.focusIndex++;
+							}
+
+							if (ui.dialog.focusIndex < 0) {
+								ui.dialog.focusIndex = ui.dialog.focusableArray.length - 1;
+							} else if (ui.dialog.focusIndex > ui.dialog.focusableArray.length - 1) {
+								ui.dialog.focusIndex = 0;
+							}
+
+							var itemToFocus = ui.dialog.focusableArray[ui.dialog.focusIndex];
+							if (itemToFocus.style.display !== 'none') {
+								itemToFocus.focus();
+							} else {
+								tabFocus(e);
+							}
+						}
+					};
+
+					$(document).on('keydown', function (e) {
+						if (ui.dialog.active) {
+							if (e.keyCode === 27) {
+								ui.dialog.hide();
+							} else if (e.keyCode === 9) {
+								tabFocus(e);
+							}
+						}
+					});
 				}
 
+				this.focusableArray = toArray($('a, input, button, [tabindex]:not([tabindex="-1"])', this.el));
 				return this.el;
 			},
 
-			setTitle: function(title) {
+			setTitle: function (title) {
 				$('.title', this.el).text(title);
 			},
 
-			setText: function(text) {
+			setText: function (text) {
 				$('.body p', this.el).html(text);
 			},
 
@@ -432,6 +478,8 @@
 				if (!this.active) {
 					this.active = true;
 					this.el.scrollTop(0).fadeIn(100);
+					this.focusIndex = this.focusableArray.indexOf(this.dialog);
+					this.dialog.focus();
 				}
 			},
 
@@ -439,6 +487,9 @@
 				if (this.active) {
 					this.active = false;
 					this.el.fadeOut(100).trigger('hidden.dialog');
+					if (this.originator) {
+						this.originator.focus();
+					}
 				}
 			}
 		}
