b2eccb47561a2ad95a6879dddeb4af2925e8dde4
Jet Fontanilla <jet.fontanilla@englishcentral.com>
2017-09-15 08:57:33 -0400

    Use correct filename in recorder.js from mic demo
    Close #373

--- microphone/scripts/recorder.js
+++ microphone/scripts/recorder.js
@@ -19,7 +19,7 @@ DEALINGS IN THE SOFTWARE.
 
 (function(window){
 
-  var WORKER_PATH = 'scripts/recorderWorker.js';
+  var WORKER_PATH = 'scripts/recorderworker.js';
 
   var Recorder = function(source, cfg){
     var config = cfg || {};
